223637ce3db915212f88436e37c76bf20447ff68
Timothy <t.vanwonterghem@option.com>
2017-06-28 13:51:19 +0200

    LuvitRED libuv patches
    * return udp destination address (breaks api)
    * use gettimeofday for cond_timedwait
    * gpio adc interrupt fix
    * remove debug in configure.ac
    
    Notes:
    
    UV_INTERRUPT changed to UV_PRIORITIZED
    UV_POLLPRI + UV_PRIORITIZED implemented in 1.14
    C++ style comments are not allowed in ISO C90
    Using gettimeofday over uv__hrtime or clock_gettime(CLOCK_REALTIME)

--- include/uv.h
+++ include/uv.h
@@ -611,14 +611,16 @@ enum uv_udp_flags {
    * the beginning of the buffer allocated by alloc_cb - so the buffer provided
    * must not be freed by the recv_cb callback.
    */
-  UV_UDP_MMSG_CHUNK = 8
+  UV_UDP_MMSG_CHUNK = 8,
+  UV_UDP_PKTINFO = 32
 };
 
 typedef void (*uv_udp_send_cb)(uv_udp_send_t* req, int status);
 typedef void (*uv_udp_recv_cb)(uv_udp_t* handle,
                                ssize_t nread,
                                const uv_buf_t* buf,
-                               const struct sockaddr* addr,
+                               const struct sockaddr* dst,
+                               const struct sockaddr* src,
                                unsigned flags);
 
 /* uv_udp_t is a subclass of uv_handle_t. */
